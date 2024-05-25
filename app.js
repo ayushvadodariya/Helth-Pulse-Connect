5fc731af14e65687e643d5e3b8e54731b21db602
ayushvadodariya <ayushvadodariya12@gamil.com>
2024-05-25 13:59:05 +0530

    rm log file, wt .env-example

--- app.js
+++ app.js
@@ -2,14 +2,11 @@ require('dotenv').config();
 
 const express = require("express");
 const http = require("http");
-const fs = require('fs');
-
 
 const userRouterV1 = require('./routes/userRouteV1');
 const childRouterV1 = require('./routes/childRouteV1');
 
 const { checkForAuthenticationToken } = require('./middlewares/authentication');
-
 const { connectMongoDb } = require('./connection');
 
 const app = express();
@@ -22,17 +19,9 @@ connectMongoDb(process.env.MONGO_CONNECTION_URL)
 
 app.use(express.json());
 
-app.use((req, res, next) => {
-  const msg = `${req.method} \ ${req.url} ${req.hostname} ${Date.now()}\n`;
-
-  fs.appendFile('log.txt', msg, () => { });
-  next();
-});
-
-// User - Route
 app.use('/api/v1/user', checkForAuthenticationToken(), userRouterV1);
 app.use('/api/v1/child', childRouterV1);
 
 
 
-server.listen(PORT, () => console.log(`server started at http://localhost:8001/`));
+server.listen(PORT, () => console.log(`server started at https://helth-pulse-connect.onrender.com`));
